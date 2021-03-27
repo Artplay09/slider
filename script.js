c08a750ee3b515176f06ca31eae5c7c040991f2a
Artplay09 <74101697+Artplay09@users.noreply.github.com>
2021-03-27 17:43:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const leftArrow = document.getElementsByClassName('arrow left')[0];
+const rightArrow = document.getElementsByClassName('arrow right')[0];
+
+const slide = document.getElementsByClassName('slide');
