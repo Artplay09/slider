b075baa6ff4df13405e74ffa5854996ab9ab9d7d
Artplay09 <74101697+Artplay09@users.noreply.github.com>
2021-04-10 17:19:39 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const leftArrow = document.getElementsByClassName('arrow left')[0];
 const rightArrow = document.getElementsByClassName('arrow right')[0];
 
 const slides = document.getElementsByClassName('slide'); 
-const dots = document.getElementsByClassName('dots');
+const dots = document.getElementsByClassName('dots')[0];
 const dot = document.getElementsByClassName('dot');
 let index = 0;
 
@@ -13,7 +13,7 @@ function showSlide(i){
    }
    slides[i].classList.add('active');
    index = i;
-//    dots[index].checked = true;
+   dot[index].checked = true;
 }
 leftArrow.addEventListener('click', () => {
     let currentIndex = index === 0 ? slides.length - 1 : index -1;
@@ -23,3 +23,23 @@ rightArrow.addEventListener('click', () => {
     let currentIndex = index === slides.length -1 ? 0 : index +1;
     showSlide(currentIndex);
 })
+
+dots.addEventListener('click', (e) => {
+    for(let i = 0; i < dot.length; i++) {
+        if(dot[i] === e.target){
+            showSlide(i);
+            break;
+        }
+    }
+})
+ //тупо я не помню
+// setInterval( () => {
+//     console.log(123)
+// }, 3000)
+
+
+// Код для переключения автоматом.
+//  setInterval( () => {
+//     let currentIndex = index === slides.length -1 ? 0 : index +1;
+//    showSlide(currentIndex);
+//  }, 3000) // 3000 это милисекунды.Надо запомнить.
